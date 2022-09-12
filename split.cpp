ce083a6078f99155dd39eda1abfd23065eba5668
Anton Ramirez Padilla <antonram@usc.edu>
2022-09-12 21:01:49 +0000

    Problem 1 submission

--- split.cpp
+++ split.cpp
@@ -14,13 +14,51 @@ the function below should be the only one in this file.
 
 /* Add a prototype for a helper function here if you need */
 
+void splitHelper(Node*& in, Node*& odds, Node*& evens) {
+  
+// if in is now empty, no nodes will be assigned to the other lists
+	if(in == nullptr) {
+		return;
+	}
+
+	// if the current value is even, we add it to the evens linked list
+	if(in->value%2 == 0) {
+		addToList(evens, in);
+		splitHelper(in, odds, evens);
+	}
+
+// else, we add it to the odds linked list
+	else {
+		addToList(odds, in);
+		splitHelper(in, odds, evens);
+	}
+
+}
+
 void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
 // WRITE YOUR CODE HERE
-
+  splitHelper(in, odds, evens);
+  in = nullptr;
 }
 
+
+
 /* If you needed a helper function, write it here */
+
+void addToList(Node*& target, Node*& in) {
+	// if nothing in the block we are looking at for target linked list
+	if(target == nullptr) {
+		// we update target linked list, move input along to the next, and remove connection from target to the rest of the items in input linked list
+		target = in;
+		in = in->next;
+		target->next = nullptr;
+	}
+	else {
+		// traverse target linked list until we find open spot
+		addToList(target->next, in);
+	}
+}
 
 // WRITE YOUR CODE HERE
---
+++ split_test.cpp
@@ -0,0 +1,38 @@
+#include "split.h"
+#include <iostream>
+
+int main(){
+	Node* node5 = new Node(5, nullptr);
+	Node* node4 = new Node(4, node5);
+	Node* node3 = new Node(3, node4);
+	Node* node2 = new Node(2, node3);
+	Node* node1 = new Node(1, node2);
+
+	Node* in = node1;
+
+	Node* evens = nullptr;
+	Node* odds = nullptr;
+
+	split(in, odds, evens);
+	std::cout << odds << " " << node1 << std::endl;
+
+	while(odds != nullptr) {
+		std::cout << odds->value << " ";
+		odds = odds->next;
+	}
+
+	std::cout << std::endl;
+	
+	while(evens != nullptr) {
+		std::cout << evens->value << " ";
+		evens = evens->next;
+	}
+
+	delete node1;
+	delete node2;
+	delete node3;
+	delete node4;
+	delete node5;
+
+	return 0;			
+}
